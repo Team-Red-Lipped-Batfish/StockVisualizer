146c2bad27eb2822094c18b601d3d1519dedc12c
Connor Bovino <connor.bovino@gmail.com>
2020-01-27 12:20:32 -0500

    Google Oauth

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,6 +1,23 @@
 const express = require('express');
 
 const router = express.Router();
+const passport = require('passport');
 
+
+// Handle the google redirect
+router.get('/google/callback', passport.authenticate('google'), (req, res, next) => {
+  // handle with passport
+  next();
+});
+
+//login with google
+router.get('/google', passport.authenticate('google', {
+  scope: ['profile']
+}));
+
+router.get('/logout', (req, res, next) => {
+  // handle with passport
+  next();
+})
 
 module.exports = router;
